7d649be8cb5ebae539f31b419d7fabe895be06f0
MihikaDevireddy <mdevireddy@cherrycreekschools.org>
2020-09-04 12:13:07 -0600

    'AngryBirdsStage6'

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState = "onsling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,12 +70,15 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState !=="launched"){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY}); 
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched"
 }
 
 function keyPressed(){
